f06a86e293b8ba1ebb88f323d0163b7708fa2445
shivank <shivank.pasi@somaiya.edu>
2024-01-03 01:28:06 +0530

    update local dburl

--- DB.js
+++ DB.js
@@ -1,6 +1,6 @@
 const { default: mongoose } = require("mongoose");
 
-const DB_URL = process.env.MONGODB_URL || "mongodb://127.0.0.1:27017/PG";
+const DB_URL = process.env.MONGODB_URL || "mongodb://127.0.0.1:27017";
 async function Connect_DB() {
   await mongoose
     .connect(DB_URL + "/PG")
