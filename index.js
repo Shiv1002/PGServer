239e82e21661a54c61a395cf7be0e6d15a878a03
shivank <shivank.pasi@somaiya.edu>
2024-01-10 20:30:34 +0530

    update:encryption of data added

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 const PG = require("./models/PG");
 const Connect_DB = require("./DB");
+require("dotenv").config();
+
+const {
+  getEncryptedEmail,
+  getDecryptedPass,
+  getEncryptedPassword,
+} = require("./controllers/getEncDec.js");
+
 const app = express();
 const PORT = process.env.PORT || 1212;
 
@@ -23,24 +31,29 @@ app.get("/", (req, res) => {
 
 app.post("/get-password", async (req, res) => {
   const { email } = req.body;
-  await PG.findOne({ email: email })
+  const encEmail = getEncryptedEmail(email);
+  await PG.findOne({ email: encEmail })
     .then((data) => {
-      res.status(200).json(data);
+      res.status(200).json(getDecryptedPass(data));
     })
     .catch((e) => res.status(404).json(e.message));
 });
 
 app.post("/add-password", async (req, res) => {
-  const { email, password } = req.body;
+  let { email, password } = req.body;
+
+  const encEmail = getEncryptedEmail(email);
+  const encPass = getEncryptedPassword(password);
+
   console.log("➕ password");
 
-  await PG.updateOne({ email: email }, { $push: { passwords: password } })
+  await PG.updateOne({ email: encEmail }, { $push: { passwords: encPass } })
     .then(async (data) => {
       let rs = data;
       if (!data.modifiedCount) {
         const newDoc = new PG({
-          email: email,
-          passwords: [password],
+          email: encEmail,
+          passwords: [encPass],
         });
         await newDoc.save();
         rs = { modifiedCount: 1 };
